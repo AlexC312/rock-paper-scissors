5948784a2fd78b4e3169c0faa088de781d6e0c81
alex <alex@smth.com>
2022-06-09 15:22:12 +0300

    Add playRound function
    Added function which plays one round

--- script.js
+++ script.js
@@ -8,3 +8,40 @@ function computerPlay() {
   else
     return 'Scissors';
 }
+
+function playRound(playerSelection, computerSelection) {
+
+  if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+
+    return `It's a draw! Both players chose ${playerSelection[0].toLowerCase() + playerSelection.slice(1).toLowerCase()}`;
+  }
+  else if (playerSelection.toLowerCase() === 'rock') {
+    if (computerSelection.toLowerCase() === 'paper') {
+
+      return 'Computer wins! Paper beats Rock';
+    }
+    else if (computerSelection.toLowerCase() === 'scissors') {
+      return 'You win! Rock beats Scissors';
+    }
+  }
+  else if (playerSelection.toLowerCase() === 'paper') {
+    if (computerSelection.toLowerCase() === 'rock') {
+
+      return 'You win! Paper beats Rock';
+    }
+    else if (computerSelection.toLowerCase() === 'scissors') {
+      return 'Computer wins! Scissors beats paper';
+    }
+  }
+  else if (playerSelection.toLowerCase() === 'scissors') {
+    if (computerSelection.toLowerCase() === 'paper') {
+
+      return 'You win! Scissors beats Paper';
+    }
+    else if (computerSelection.toLowerCase() === 'rock') {
+      return 'Computer wins! Rock beats Scissors';
+    }
+  }
+  else
+    return 'Invalid choice!!!';
+}
